4c7a5efd9129819245bbf4612b66b1427572523f
Gregorio Pérez Bernal <60147053+gperezb12@users.noreply.github.com>
2021-05-27 11:47:05 -0500

    Add files via upload

--- src/regex-posix/string/Main.hs
+++ src/regex-posix/string/Main.hs
@@ -1,8 +1,38 @@
-import Criterion.Measurement
+--https://chrisdone.com/posts/measuring-duration-in-haskell/
+
 import Text.Regex.Posix
 import System.IO
---Reads the input into a FA and takes the name of the file to be read as what the user inputs
+import Text.Read (readMaybe)
+import Data.List ( nub )
+import Data.Set ( Set )
+import Data.String
+import Data.Typeable 
+import System.CPUTime
+import Text.Printf
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Exception
+import Formatting
+import Formatting.Clock
+import System.Clock
+
+
+data PosixString = PosixString String String 
+    deriving (Eq, Ord)
+
+
+isRegEx:: String -> String -> Bool
+isRegEx x y = y =~ x :: Bool
+
 main :: IO ()
 main = do 
-    s <- readFile "tx1.txt"
-    print s
+    file <- getLine
+    start <- getTime Monotonic
+    let arr = words file
+    input <- readFile (head(arr))
+    let source = last(arr)
+    let b = input =~ source :: Bool 
+    end <- getTime Monotonic
+    print b
+    putStrLn " "
+    fprint (timeSpecs ) start end
+
