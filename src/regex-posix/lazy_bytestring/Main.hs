dac68d83de84967715ccf47b5153235c51bf0383
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 15:43:26 -0500

    Add files via upload

--- src/regex-posix/lazy_bytestring/Main.hs
+++ src/regex-posix/lazy_bytestring/Main.hs
@@ -1,29 +1,29 @@
 module Main where
 
---import Text.Regex.Posix.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L
 import Text.Regex.Posix
-import System.IO
-import Text.Read (readMaybe)
-import Data.List ( nub )
-import Data.Typeable  
 import System.Environment (getArgs)
 import Data.Time
-
-
 
 isRegEx:: L.ByteString ->  L.ByteString -> Bool
 isRegEx x y =  y =~ x :: Bool
 
+boolToString :: Bool -> String
+boolToString True  = "True"
+boolToString False = "False"
+
+printaux ::  Bool -> String
+printaux a = "Result: " ++ boolToString a
+
 main :: IO ()
 main = do 
     start <- getCurrentTime
-    file <- getArgs
+    file  <- getArgs
     input <- readFile (head(file))
-    let source = L.pack (last(file))
+    let source   = L.pack (last(file))
     let inputaux = L.pack input
-    let match = isRegEx source inputaux
-    print match
+    let match    = isRegEx source inputaux
+    print (printaux match)
     stop <- getCurrentTime
-    putStrLn " "
+    print "Time: "
     print $ diffUTCTime stop start
