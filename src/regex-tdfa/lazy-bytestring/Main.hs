e96f8ad7a311331fe10b23d89a87dc1b650dba3e
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 15:44:51 -0500

    Add files via upload

--- src/regex-tdfa/lazy-bytestring/Main.hs
+++ src/regex-tdfa/lazy-bytestring/Main.hs
@@ -1,8 +1,29 @@
-import Criterion.Measurement
-import Text.Regex.Posix
-import System.IO
---Reads the input into a FA and takes the name of the file to be read as what the user inputs
+module Main where
+
+import Text.Regex.TDFA
+import qualified Data.ByteString.Lazy.Char8 as L
+import System.Environment (getArgs)
+import Data.Time
+
+isRegEx:: L.ByteString ->  L.ByteString -> Bool
+isRegEx x y =  y =~ x :: Bool
+
+boolToString :: Bool -> String
+boolToString True  = "True"
+boolToString False = "False"
+
+printaux ::  Bool -> String
+printaux a = "Result: " ++ boolToString a
+
 main :: IO ()
 main = do 
-    s <- readFile "tx1.txt"
-    print s
+    start <- getCurrentTime
+    file  <- getArgs
+    input <- readFile (head(file))
+    let source    = L.pack (last(file))
+    let inputaux  = L.pack input
+    let match     = isRegEx source inputaux
+    print (printaux match)
+    stop <- getCurrentTime 
+    print "Time: " 
+    print $ diffUTCTime stop start
