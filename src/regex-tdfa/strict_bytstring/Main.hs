5914fb7806383df7f7efa2d2d0137946a0319a7d
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 15:44:23 -0500

    Add files via upload

--- src/regex-tdfa/strict_bytstring/Main.hs
+++ src/regex-tdfa/strict_bytstring/Main.hs
@@ -1,8 +1,29 @@
-import Criterion.Measurement
-import Text.Regex.Posix
-import System.IO
---Reads the input into a FA and takes the name of the file to be read as what the user inputs
+module Main where
+
+import Text.Regex.TDFA
+import qualified Data.ByteString.Char8 as S
+import System.Environment (getArgs)
+import Data.Time
+
+isRegEx:: S.ByteString ->  S.ByteString -> Bool
+isRegEx x y =  y =~ x :: Bool
+
+boolToString :: Bool -> String
+boolToString True  = "True"
+boolToString False = "False"
+
+printaux ::  Bool -> String
+printaux a = "Result: " ++ boolToString a
+
 main :: IO ()
 main = do 
-    s <- readFile "tx1.txt"
-    print s
+    start <- getCurrentTime
+    file  <- getArgs
+    input <- readFile (head(file))
+    let source   = S.pack (last(file))
+    let inputaux = S.pack input
+    let match    = isRegEx source inputaux
+    print (printaux match)
+    stop <- getCurrentTime
+    print "Time: "
+    print $ diffUTCTime stop start
