92f30623b07466a3f3d3cababe69ac274d633868
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 11:57:16 -0500

    Add files via upload

--- src/regex-posix/lazy_bytestring/Main.hs
+++ src/regex-posix/lazy_bytestring/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
-import Text.Regex.Posix.ByteString.Lazy
+--import Text.Regex.Posix.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L
+import Text.Regex.Posix
+import System.IO
+import Text.Read (readMaybe)
+import Data.List ( nub )
+import Data.Typeable  
+import System.Environment (getArgs)
+import Data.Time
+
+
+
+isRegEx:: L.ByteString ->  L.ByteString -> Bool
+isRegEx x y =  y =~ x :: Bool
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    start <- getCurrentTime
+    file <- getArgs
+    input <- readFile (head(file))
+    let source = L.pack (last(file))
+    let inputaux = L.pack input
+    let match = isRegEx source inputaux
+    print match
+    stop <- getCurrentTime
+    putStrLn " "
+    print $ diffUTCTime stop start
