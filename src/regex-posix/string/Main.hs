f218149e25936e5045246759a9908d0e175b3e0b
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 16:00:11 -0500

    Update Main.hs

--- src/regex-posix/string/Main.hs
+++ src/regex-posix/string/Main.hs
@@ -1,21 +1,24 @@
 module Main where
---https://chrisdone.com/posts/measuring-duration-in-haskell/
-
+--Libraries needed
 import Text.Regex.Posix.String
 import Text.Regex.Posix
 import System.Environment (getArgs)
 import Data.Time
-
+--This function recieves two parameters of type string and returns a boolean.
+--It determines whether the regular expression matches the string contained in the text file.
 isRegEx:: String -> String -> Bool
 isRegEx x y = y =~ x :: Bool
 
+--Auxiliary function which recieves a boolean and writes it as a string
 boolToString :: Bool -> String
 boolToString True  = "True"
 boolToString False = "False"
 
+--Auxiliary function which recieves a boolean and return a string chain of the format required.
 printaux ::  Bool -> String
 printaux a = "Result: " ++ boolToString a
 
+--The main function which reads from the command the arguments, text file name and the regular expression, reads the text file and prints the expected output.
 main :: IO ()
 main = do 
     start <- getCurrentTime
