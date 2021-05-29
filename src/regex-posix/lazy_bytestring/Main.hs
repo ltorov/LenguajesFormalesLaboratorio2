8bf1cdee2d87e3da76005552cd24b66f565795c4
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 16:03:21 -0500

    Update Main.hs

--- src/regex-posix/lazy_bytestring/Main.hs
+++ src/regex-posix/lazy_bytestring/Main.hs
@@ -1,20 +1,26 @@
 module Main where
 
+--Libraries needed
 import qualified Data.ByteString.Lazy.Char8 as L
 import Text.Regex.Posix
 import System.Environment (getArgs)
 import Data.Time
 
+--This function recieves two parameters of type lazy bytestring and returns a boolean.
+--It determines whether the regular expression matches the lazy bytestring contained in the text file.
 isRegEx:: L.ByteString ->  L.ByteString -> Bool
 isRegEx x y =  y =~ x :: Bool
 
+--Auxiliary function which recieves a boolean and writes it as a string.
 boolToString :: Bool -> String
 boolToString True  = "True"
 boolToString False = "False"
 
+--Auxiliary function which recieves a boolean and return a string chain of the format required.
 printaux ::  Bool -> String
 printaux a = "Result: " ++ boolToString a
 
+--The main function which reads from the command the arguments, text file name and the regular expression, reads the text file and prints the expected output.
 main :: IO ()
 main = do 
     start <- getCurrentTime
