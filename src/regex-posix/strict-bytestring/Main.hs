0ae4ee32a9d69f12c64123db5a440b68273f494f
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 15:42:56 -0500

    Add files via upload

--- src/regex-posix/strict-bytestring/Main.hs
+++ src/regex-posix/strict-bytestring/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Text.Regex.Posix.ByteString
+import qualified Data.ByteString.Char8 as S
+import Text.Regex.Posix
+import System.Environment (getArgs)
+import Data.Time
+
+
+isRegEx:: S.ByteString ->  S.ByteString -> Bool
+isRegEx x y =  y =~ x :: Bool
+
+boolToString :: Bool -> String
+boolToString True  = "True"
+boolToString False = "False"
+
+printaux ::  Bool -> String
+printaux a = "Result: " ++ boolToString a
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    start <- getCurrentTime
+    file  <- getArgs
+    input <- readFile (head(file))
+    let source   = S.pack (last(file))
+    let inputaux = S.pack input
+    let match    = isRegEx source inputaux
+    print (printaux match)
+    stop <- getCurrentTime
+    print "Time: "
+    print $ diffUTCTime stop start
