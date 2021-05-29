1a4d3e407a314a87423f5baf18a5028a1442bee5
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 15:42:05 -0500

    Add files via upload

--- src/regex-posix/string/Main.hs
+++ src/regex-posix/string/Main.hs
@@ -3,24 +3,27 @@ module Main where
 
 import Text.Regex.Posix.String
 import Text.Regex.Posix
-import System.IO
-import Text.Read (readMaybe)
-import Data.List ( nub )
-import Data.Typeable  
 import System.Environment (getArgs)
 import Data.Time
 
 isRegEx:: String -> String -> Bool
 isRegEx x y = y =~ x :: Bool
 
+boolToString :: Bool -> String
+boolToString True  = "True"
+boolToString False = "False"
+
+printaux ::  Bool -> String
+printaux a = "Result: " ++ boolToString a
+
 main :: IO ()
 main = do 
     start <- getCurrentTime
-    file <- getArgs
+    file  <- getArgs
     input <- readFile (head(file))
     let source = last(file)
-    let match = isRegEx source input 
-    print match
+    let match  = isRegEx source input 
+    print (printaux match)
     stop <- getCurrentTime
-    putStrLn " "
+    print "Time: "
     print $ diffUTCTime stop start
