3a3ae1f47895688490c3c79858686d78eb99795d
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-29 16:06:48 -0500

    Update Main.hs

--- src/regex-tdfa/strict_bytstring/Main.hs
+++ src/regex-tdfa/strict_bytstring/Main.hs
@@ -1,20 +1,26 @@
 module Main where
 
+--Libraries needed
 import Text.Regex.TDFA
 import qualified Data.ByteString.Char8 as S
 import System.Environment (getArgs)
 import Data.Time
 
+--This function recieves two parameters of type strict bytestring and returns a boolean.
+--It determines whether the regular expression matches the strict bytestring contained in the text file using the regex tdfa library.
 isRegEx:: S.ByteString ->  S.ByteString -> Bool
 isRegEx x y =  y =~ x :: Bool
 
+--Auxiliary function which recieves a boolean and writes it as a string
 boolToString :: Bool -> String
 boolToString True  = "True"
 boolToString False = "False"
 
+--Auxiliary function which recieves a boolean and return a string chain of the format required.
 printaux ::  Bool -> String
 printaux a = "Result: " ++ boolToString a
 
+--The main function which reads from the command the arguments, text file name and the regular expression, reads the text file and prints the expected output.
 main :: IO ()
 main = do 
     start <- getCurrentTime
