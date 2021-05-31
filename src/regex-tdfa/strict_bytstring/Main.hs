2d72eb190998e3037d22a4c8eb2508b5695b1a00
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-05-31 11:39:44 -0400

    Update Main.hs

--- src/regex-tdfa/strict_bytstring/Main.hs
+++ src/regex-tdfa/strict_bytstring/Main.hs
@@ -8,8 +8,8 @@ import Data.Time
 
 --This function recieves two parameters of type strict bytestring and returns a boolean.
 --It determines whether the regular expression matches the strict bytestring contained in the text file using the regex tdfa library.
-isRegEx:: S.ByteString ->  S.ByteString -> Bool
-isRegEx x y =  y =~ x :: Bool
+matchRegEx:: S.ByteString ->  S.ByteString -> Bool
+matchRegEx x y =  y =~ x :: Bool
 
 --Auxiliary function which recieves a boolean and writes it as a string
 boolToString :: Bool -> String
@@ -28,7 +28,7 @@ main = do
     input <- readFile (head(file))
     let source   = S.pack (last(file))
     let inputaux = S.pack input
-    let match    = isRegEx source inputaux
+    let match    = matchRegEx source inputaux
     print (printaux match)
     stop <- getCurrentTime
     print "Time: "
